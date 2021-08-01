f349cfafb9e1f386e29cba761bc621b2b9a80857
ravi04 <rs1313069@gmail.com>
2021-08-01 16:03:16 +0530

    all the activity will respond as we click it and actionBar name is change for all the activity.

--- app/src/main/java/com/example/miwokapp/MainActivity.java
+++ app/src/main/java/com/example/miwokapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.miwokapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void openNumberList(View v){
-        Intent intent = new Intent(this , NumbersActivity.class);
-        startActivity(intent);
+        TextView openNumbersActivity = findViewById(R.id.numbers);
+        TextView openFamilyActivity = findViewById(R.id.family);
+        TextView openColorsActivity = findViewById(R.id.colors);
+        TextView openPhrasesActivity = findViewById(R.id.phrases);
+
+        openNumbersActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent numberIntent = new Intent(MainActivity.this,NumbersActivity.class);
+                startActivity(numberIntent);
+            }
+        });
+        openFamilyActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent familyIntent = new Intent(MainActivity.this,FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+        openColorsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent colorIntent = new Intent(MainActivity.this,ColorsActivity.class);
+                startActivity(colorIntent);
+            }
+        });
+        openPhrasesActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phrasesIntent = new Intent(MainActivity.this,PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
     }
 }

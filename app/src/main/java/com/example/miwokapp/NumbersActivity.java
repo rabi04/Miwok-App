2485f11e4f8b8721fcc9c69dd4619cb0cc7eec45
ravi04 <rs1313069@gmail.com>
2021-08-07 00:11:56 +0530

    display word in listView using ArrayAdapter in NumbersActivity.

--- app/src/main/java/com/example/miwokapp/NumbersActivity.java
+++ app/src/main/java/com/example/miwokapp/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.miwokapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,12 +22,16 @@ public class NumbersActivity extends AppCompatActivity {
         number.add("three");
         number.add("four");
         number.add("five");
-        LinearLayout rootView = findViewById(R.id.rootView);
+     /*   LinearLayout rootView = findViewById(R.id.rootView);
         for(int i = 0; i < number.size() ; i++) {
             TextView textView = new TextView(this);
             textView.setText(number.get(i));
             textView.setPadding(10,10,10,10);
             rootView.addView(textView);
-        }
+        }        */
+        // Using listview and arrayadapter for memory optimization.
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,number);
+        ListView listview = findViewById(R.id.list);
+        listview.setAdapter(itemAdapter);
     }
 }
